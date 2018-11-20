74dfe3ee98ffdc6e62a287abd0e9dcc197abda2c
Trí <asylumcrow.code@gmail.com>
2018-11-20 10:15:13 +0700

    Homework 7 - Need fix more

---
+++ Week 1/Assets/Scripts/Game/PlaneMissileMoving.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlaneMissileMoving : MonoBehaviour {
+
+    private float speed = 1f;
+    private float rotateSpeed = 200f;
+    private Transform target;
+    private Rigidbody2D rigidbody2D;
+    
+    // Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("EnemyPlane").transform;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (Vector3)target.position - this.transform.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rigidbody2D.angularVelocity = -rotateAmount * rotateSpeed;
+
+        this.transform.position += this.transform.up * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "EnemyPlane")
+        {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Week 1/Assets/Scripts/Game/PlaneMissileShooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMissileShooting : MonoBehaviour {
+
+    public Transform firePosition;
+    public GameObject bulletPrefab;
+    private float shootingDelay = 2f;
+    private float shootingDelayTimer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shootingDelayTimer += Time.deltaTime;
+        if (shootingDelayTimer >= shootingDelay)
+        {
+            shootingDelayTimer = 0;
+            Shooting();
+        }
+    }
+
+    void Shooting()
+    {
+        Instantiate(bulletPrefab, firePosition.position, firePosition.rotation);
+    }
+}
